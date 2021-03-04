25dd146c0702bc6e980045d268d64ca4e44c58c5
jp5485 <78568307+jp5485@users.noreply.github.com>
2021-03-04 11:54:16 -0500

    cuisine types added, all 80 of them

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,80 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoianA1NDg1IiwiYSI6ImNrM2Y3bTN5cDAxemEzaG5ubjM1NHpzdXIifQ.1ZnEtqoWaJQRTjp7E2GgGA';
 // function to translate NYC land use codes into a color and a description
-var LandUseLookup = (code) => {
-  switch (code) {
-    case 1:
-      return {
-        color: '#f4f455',
-        description: '1 & 2 Family',
-      };
-    case 2:
-      return {
-        color: '#f7d496',
-        description: 'Multifamily Walk-up',
-      };
-    case 3:
-      return {
-        color: '#FF9900',
-        description: 'Multifamily Elevator',
-      };
-    case 4:
-      return {
-        color: '#f7cabf',
-        description: 'Mixed Res. & Commercial',
-      };
-    case 5:
-      return {
-        color: '#ea6661',
-        description: 'Commercial & Office',
-      };
-    case 6:
-      return {
-        color: '#d36ff4',
-        description: 'Industrial & Manufacturing',
-      };
-    case 7:
-      return {
-        color: '#dac0e8',
-        description: 'Transportation & Utility',
-      };
-    case 8:
-      return {
-        color: '#5CA2D1',
-        description: 'Public Facilities & Institutions',
-      };
-    case 9:
-      return {
-        color: '#8ece7c',
-        description: 'Open Space & Outdoor Recreation',
-      };
-    case 10:
-      return {
-        color: '#bab8b6',
-        description: 'Parking Facilities',
-      };
-    case 11:
-      return {
-        color: '#5f5f60',
-        description: 'Vacant Land',
-      };
-    case 12:
-      return {
-        color: '#5f5f60',
-        description: 'Other',
-      };
-    default:
-      return {
-        color: '#5f5f60',
-        description: 'Other',
-      };
-  }
-};
+// var CuisineLookup = (code) => {
+//   switch (code) {
+//     case 1:
+//       return {
+//         color: '#f4f455',
+//         description: '1 & 2 Family',
+//       };
+//     case 2:
+//       return {
+//         color: '#f7d496',
+//         description: 'Multifamily Walk-up',
+//       };
+//     case 3:
+//       return {
+//         color: '#FF9900',
+//         description: 'Multifamily Elevator',
+//       };
+//     case 4:
+//       return {
+//         color: '#f7cabf',
+//         description: 'Mixed Res. & Commercial',
+//       };
+//     case 5:
+//       return {
+//         color: '#ea6661',
+//         description: 'Commercial & Office',
+//       };
+//     case 6:
+//       return {
+//         color: '#d36ff4',
+//         description: 'Industrial & Manufacturing',
+//       };
+//     case 7:
+//       return {
+//         color: '#dac0e8',
+//         description: 'Transportation & Utility',
+//       };
+//     case 8:
+//       return {
+//         color: '#5CA2D1',
+//         description: 'Public Facilities & Institutions',
+//       };
+//     case 9:
+//       return {
+//         color: '#8ece7c',
+//         description: 'Open Space & Outdoor Recreation',
+//       };
+//     case 10:
+//       return {
+//         color: '#bab8b6',
+//         description: 'Parking Facilities',
+//       };
+//     case 11:
+//       return {
+//         color: '#5f5f60',
+//         description: 'Vacant Land',
+//       };
+//     case 12:
+//       return {
+//         color: '#5f5f60',
+//         description: 'Other',
+//       };
+//     default:
+//       return {
+//         color: '#5f5f60',
+//         description: 'Other',
+//       };
+//   }
+// };
 
 var map = new mapboxgl.Map({
   container: 'mapContainer', // container ID
   style: 'mapbox://styles/mapbox/light-v10', // style URL
   center: [-73.916949,40.682562], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  zoom: 11 // starting zoom
 });
 
 map.on('style.load', function () {
@@ -86,60 +86,335 @@ map.on('style.load', function () {
 
   // add a layer to style and display the source
   map.addLayer({
-    'id': 'brooklyn-cd6-fill',
+    'id': 'restaurants-fill',
     'type': 'fill',
-    'source': 'brooklyn-cd6',
+    'source': 'open-restaurants',
     'layout': {},
     'paint': {
       'fill-color': {
         type: 'categorical',
-        property: 'LandUse',
+        property: 'CUISINE_DESCRIPTION',
         stops: [
           [
-            '01',
-            LandUseLookup(1).color,
+            'Chinese',
+            "#BBB572",
           ],
           [
-            '02',
-            LandUseLookup(2).color,
+            'American',
+            "#93828E",
           ],
           [
-            '03',
-            LandUseLookup(3).color,
+            'Jewish/Kosher',
+            "#1E2D24",
           ],
           [
-            '04',
-            LandUseLookup(4).color,
+            'Italian',
+            "#F1F2AA",
           ],
           [
-            '05',
-            LandUseLookup(5).color,
+            'Japanese',
+            "#2bb4d6",
           ],
           [
-            '06',
-            LandUseLookup(6).color,
+            'Cakes, Cupcakes, Desserts',
+            "#d18ecc",
           ],
           [
-            '07',
-            LandUseLookup(7).color,
+            'Pizza',
+            "#f2bb4e",
           ],
           [
-            '08',
-            LandUseLookup(8).color,
+            'Latin American (Central and South America)',
+            "#1a6330",
           ],
           [
-            '09',
-            LandUseLookup(9).color,
+            'Tex-Mex',
+            "#f0a607",
           ],
           [
-            '10',
-            LandUseLookup(10).color,
+            'Sandwiches',
+            "#f0a607",
           ],
           [
-            '11',
-            LandUseLookup(11).color,
+            'Mexican',
+            "#f0a607",
           ],
-
+          [
+            'Asian',
+            "#f0a607",
+          ],
+          [
+            'Sandwiches/Salads/Mixed Buffet',
+            "#f0a607",
+          ],
+          [
+            'Brazilian',
+            "#f0a607",
+          ],
+          [
+            'Thai',
+            "#f0a607",
+          ],
+          [
+            'Chicken',
+            "#f0a607",
+          ],
+          [
+            'Peruvian',
+            "#f0a607",
+          ],
+          [
+            'French',
+            "#f0a607",
+          ],
+          [
+            'Caribbean (Inc. Cuban, Puerto Rican)',
+            "#f0a607",
+          ],
+          [
+            'Spanish',
+            "#f0a607",
+          ],
+          [
+            'Donuts',
+            "#f0a607",
+          ],
+          [
+            'Vegetarian',
+            "#f0a607",
+          ],
+          [
+            'Korean',
+            "#f0a607",
+          ],
+          [
+            'Seafood',
+            "#f0a607",
+          ],
+          [
+            'Other',
+            "#f0a607",
+          ],
+          [
+            'Turkish',
+            "#f0a607",
+          ],
+          [
+            'Irish',
+            "#f0a607",
+          ],
+          [
+            'Bottled beverages, including water, sodas, juices, etc.',
+            "#f0a607",
+          ],
+          [
+            'Californian',
+            "#f0a607",
+          ],
+          [
+            'Middle Eastern',
+            "#f0a607",
+          ],
+          [
+            'Soul Food',
+            "#f0a607",
+          ],
+          [
+            'Chinese/Japanese',
+            "#f0a607",
+          ],
+          [
+            'Juice, Smoothies, Fruit Salads',
+            "#f0a607",
+          ],
+          [
+            'Steak',
+            "#f0a607",
+          ],
+          [
+            'Ice Cream, Gelato, Yogurt, Ices',
+            "#f0a607",
+          ],
+          [
+            'African',
+            "#f0a607",
+          ],
+          [
+            'Hamburgers',
+            "#f0a607",
+          ],
+          [
+            'Vietnamese/Cambodian/Malaysia',
+            "#f0a607",
+          ],
+          [
+            'Eastern European',
+            "#f0a607",
+          ],
+          [
+            'Salads',
+            "#f0a607",
+          ],
+          [
+            'Creole',
+            "#f0a607",
+          ],
+          [
+            'Russian',
+            "#f0a607",
+          ],
+          [
+            'Barbecue',
+            "#f0a607",
+          ],
+          [
+            'Afghan',
+            "#f0a607",
+          ],
+          [
+            'Mediterranean',
+            "#f0a607",
+          ],
+          [
+            'Pancakes/Waffles',
+            "#f0a607",
+          ],
+          [
+            'Armenian',
+            "#f0a607",
+          ],
+          [
+            'Hotdogs/Pretzels',
+            "#f0a607",
+          ],
+          [
+            'Continental',
+            "#f0a607",
+          ],
+          [
+            'Soups/Salads/Sandwiches',
+            "#f0a607",
+          ],
+          [
+            'Hawaiian',
+            "#f0a607",
+          ],
+          [
+            'Chilean',
+            "#f0a607",
+          ],
+          [
+            'Tapas',
+            "#f0a607",
+          ],
+          [
+            'Australian',
+            "#f0a607",
+          ],
+          [
+            'Bagels/Pretzels',
+            "#f0a607",
+          ],
+          [
+            'Portuguese',
+            "#f0a607",
+          ],
+          [
+            'Cajun',
+            "#f0a607",
+          ],
+          [
+            'Ethiopian',
+            "#f0a607",
+          ],
+          [
+            'Greek',
+            "#f0a607",
+          ],
+          [
+            'Filipino',
+            "#f0a607",
+          ],
+          [
+            'Nuts/Confectionary',
+            "#f0a607",
+          ],
+          [
+            'English',
+            "#f0a607",
+          ],
+          [
+            'Polish',
+            "#f0a607",
+          ],
+          [
+            'Fruits/Vegetables',
+            "#f0a607",
+          ],
+          [
+            'Pakistani',
+            "#f0a607",
+          ],
+          [
+            'Indonesian',
+            "#f0a607",
+          ],
+          [
+            'German',
+            "#f0a607",
+          ],
+          [
+            'Bangladeshi',
+            "#f0a607",
+          ],
+          [
+            'Egyptian',
+            "#f0a607",
+          ],
+          [
+            'Chinese/Cuban',
+            "#f0a607",
+          ],
+          [
+            'Scandinavian',
+            "#f0a607",
+          ],
+          [
+            'Iranian',
+            "#f0a607",
+          ],
+          [
+            'Moroccan',
+            "#f0a607",
+          ],
+          [
+            'Not Listed/Not Applicable',
+            "#f0a607",
+          ],
+          [
+            'Creole/Cajun',
+            "#f0a607",
+          ],
+          [
+            'Czech',
+            "#f0a607",
+          ],
+          [
+            'Soups',
+            "#f0a607",
+          ],
+          [
+            'Hotdogs',
+            "#f0a607",
+          ],
+          [
+            'Southwestern',
+            "#f0a607",
+          ],
+          [
+            'Basque',
+            "#f0a607",
+          ],
         ]
       },
       'fill-outline-color': '#ccc',
@@ -178,7 +453,7 @@ var popup = new mapboxgl.Popup({
 map.on('mousemove', function (e) {
   // query for the features under the mouse, but only in the lots layer
   var features = map.queryRenderedFeatures(e.point, {
-      layers: ['brooklyn-cd6-fill'],
+      layers: ['restaurants-fill'],
   });
 
   if (features.length > 0) {
